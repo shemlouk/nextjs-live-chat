6bf6ca629b69ab63782c46c0730b32fd7518c7a5
samuel <shemlouk@gmail.com>
2023-12-06 19:53:27 -0300

    chore: remove env

--- app/lib/api/socket.ts
+++ app/lib/api/socket.ts
@@ -1,11 +1,11 @@
 import { io } from "socket.io-client";
 
-const SOCKET_URL = process.env.NEXT_PUBLIC_SOCKET_URL;
+const API_URL = process.env.NEXT_PUBLIC_API_URL;
 
 export function initializeSocket(token: string) {
-  if (!SOCKET_URL) throw new Error("Websocket server URL is missing.");
+  if (!API_URL) throw new Error("Websocket server URL is missing.");
 
-  const socket = io(SOCKET_URL, { auth: { token } });
+  const socket = io(API_URL, { auth: { token } });
 
   socket.on("connect", () => {
     console.log("Connected to WebSocket!");
